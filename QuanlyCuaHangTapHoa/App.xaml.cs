572abe3a45077e21f6a6dfc1828defc0021e9d13
Vinh Bui <vindepzai004@gmail.com>
2025-11-18 11:06:32 +0700

    Add project files.

---
+++ QuanlyCuaHangTapHoa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuanlyCuaHangTapHoa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ QuanlyCuaHangTapHoa/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanlyCuaHangTapHoa
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
